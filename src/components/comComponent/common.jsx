f9a92f8dacd486a12d9727dce14ef7d4fcbdec72
zds <1030964891@qq.com>
2018-05-08 22:51:03 +0800

    组件添加

--- src/components/comComponent/common.jsx
+++ src/components/comComponent/common.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './common.scss';
+import { Tabs, WhiteSpace, Badge } from 'antd-mobile';
 
 export const Button = props => {
 	return (
@@ -84,18 +85,30 @@ export const Validate = (props) => {
   )
 };
 
-export const Tabs = (props) => {
+export const TopTabs = (props) => {
+  const tabs = [
+    { title: props.firstTitle || '谷歌验证' },
+    { title: props.secondTitle || '邮箱验证' },
+  ];
+
   return (
-    <div className='validate' style={props.style}>
-      <input type="number" maxLength="6" autoComplete="off"/>
-      <ul className="code-display">
-        <li><span>6</span></li>
-        <li><span>1</span></li>
-        <li><span>2</span></li>
-        <li><span>1</span></li>
-        <li><span className='number'></span></li>
-        <li><span className='number'></span></li>
-      </ul>
+    <div>
+      <Tabs tabs={tabs}
+            initialPage={props.initialPage || 1}
+            tabBarBackgroundColor='#1A1A1A'
+            tabBarActiveTextColor='#DCB276'
+            tabBarInactiveTextColor='#646464'
+            tabBarUnderlineStyle={{ borderColor:'#DCB276', width: '30%', marginLeft: '10%'}}
+            tabBarTextStyle={{fontSize: '15px'}}
+      >
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '150px', backgroundColor: '#fff' }}>
+          Content of first tab
+        </div>
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '150px', backgroundColor: '#fff' }}>
+          Content of second tab
+        </div>
+      </Tabs>
+      <WhiteSpace />
     </div>
   )
 };
