125ce068683c3ea1878a21233eb4c70497370c07
ShowPenZ <1226703023@qq.com>
2018-05-09 17:21:07 +0800

    modify

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Redirect, Switch, Route } from 'react-router-dom';
-import Index from 'views/index/Index';
+import Index from 'views/index/index';
 // import Login from 'views/login/index';
 import Register from 'views/register/Register';
 import RegisterEmail from 'views/register/RegisterEmail';
