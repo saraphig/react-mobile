c434a326e4a01644e394462f51c55baddaaa9601
ShowPenZ <1226703023@qq.com>
2018-05-11 14:20:06 +0800

    add new pages

--- src/components/userCenter/MyWallet.jsx
+++ src/components/userCenter/MyWallet.jsx
@@ -11,6 +11,8 @@ import {
 	Validate,
 	Navbars
 } from '../comComponent/common';
+import { Checkbox } from 'antd-mobile';
+const CheckboxItem = Checkbox.CheckboxItem;
 
 class MyWalletComp extends React.Component {
 	constructor(props) {
@@ -23,10 +25,23 @@ class MyWalletComp extends React.Component {
 		return (
 			<div className="myWallet">
 				<Header />
-                <Navbars title='我的资产' className='myWallet-title'/>
-                <div>
-                    
-                </div>
+				<Navbars title="我的资产" className="myWallet-title" />
+				<div className="myWallet-money">
+					<span className="myWallet-money-info guji">
+						资产总估值<i className="iconfont icon-ai44" />
+						<i className="iconfont icon-yincang" />
+					</span>
+					<span className="myWallet-money-info moneys">
+						888888.88888888&nbsp;BTC<span className="myWallet-money-yuan">
+							/¥23333.333333
+						</span>
+						{/* <span>******</span> */}
+					</span>
+					<span className="myWallet-money-info">
+						{/* 资产总估值<i className="iconfont icon-ai44" /> */}
+						<Checkbox>&nbsp;&nbsp;隐藏0资产钱包</Checkbox>
+					</span>
+				</div>
 			</div>
 		);
 	}
