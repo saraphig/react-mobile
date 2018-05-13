d1c8363b7f65ddb3e31809294480cff3c1c8d070
wangyongwen <wyw651294@163.com>
2018-05-13 11:19:00 +0800

    中英文切换

--- src/components/container/Drawers.jsx
+++ src/components/container/Drawers.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FormattedMessage, injectIntl } from 'react-intl';
 import '../comComponent/common.scss';
+import { localeList } from '../../config';
 import {
 	Tabs,
 	WhiteSpace,
@@ -34,9 +35,24 @@ const sidebars = (
 		<List.Item key={5}>
 			<FormattedMessage id={'usercenter.logout'} />
 		</List.Item>
-		<List.Item key={6}>language </List.Item>
+        <List.Item key={6} onClick={() => setLanguage()}>
+            {localStorage.getItem('language') == 'zh' ? 'English' : '中文'}
+        </List.Item>
 	</List>
 );
+const setLanguage = () => {
+    const language = localStorage.getItem('language');
+    let locale = {};
+    if (language == 'en') {
+        locale = localeList[1];
+    } else if (language == 'zh') {
+        locale = localeList[0];
+    }
+    localStorage.setItem('timezone', locale.timezone);
+    localStorage.setItem('language', locale.language);
+    localStorage.setItem('languageText', locale.languageText);
+    window.location.reload();
+};
 
 class Drawers extends React.Component {
 	constructor(props) {
@@ -46,6 +62,7 @@ class Drawers extends React.Component {
 	componentDidMount() {
 		console.log(this.props);
 	}
+    // 多语言切换
 
 	render() {
 		return (
