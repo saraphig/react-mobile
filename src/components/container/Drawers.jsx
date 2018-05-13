65050d92f9834e2c74d2e99853d52c5f836c5ee2
ShowPenZ <1226703023@qq.com>
2018-05-13 17:32:53 +0800

    modify and make new

--- src/components/container/Drawers.jsx
+++ src/components/container/Drawers.jsx
@@ -20,38 +20,47 @@ import {
 //侧栏内容框
 const sidebars = (
 	<List className="sideBars">
-		<List.Item key={1} thumb={require('assets/images/iconman.png')}>
+		<List.Item key={1} thumb={require('assets/images/iconman.png')} className='siderbar-logo'>
 			<FormattedMessage id={'usercenter.login.trade'} />
 		</List.Item>
-		<List.Item key={2}>
-			<FormattedMessage id={'public.index'} />
+		<Link to="/index">
+			<List.Item key={2}>
+				<FormattedMessage id={'public.index'} />
+			</List.Item>
+		</Link>
+		<Link to="/userCenter">
+			<List.Item key={3}>
+				<FormattedMessage id={'home.personalCenter'} />
+			</List.Item>
+		</Link>
+		<Link to="/myWallet">
+			<List.Item key={4}>
+				<FormattedMessage id={'home.assets'} />
+			</List.Item>
+		</Link>
+		<Link to="/login">
+			<List.Item key={5}>
+				<FormattedMessage id={'usercenter.logout'} />
+			</List.Item>
+		</Link>
+
+		<List.Item key={6} onClick={() => setLanguage()}>
+			{localStorage.getItem('language') == 'zh' ? 'English' : '中文'}
 		</List.Item>
-		<List.Item key={3}>
-			<FormattedMessage id={'home.personalCenter'} />
-		</List.Item>
-		<List.Item key={4}>
-			<FormattedMessage id={'home.assets'} />
-		</List.Item>
-		<List.Item key={5}>
-			<FormattedMessage id={'usercenter.logout'} />
-		</List.Item>
-        <List.Item key={6} onClick={() => setLanguage()}>
-            {localStorage.getItem('language') == 'zh' ? 'English' : '中文'}
-        </List.Item>
 	</List>
 );
 const setLanguage = () => {
-    const language = localStorage.getItem('language');
-    let locale = {};
-    if (language == 'en') {
-        locale = localeList[1];
-    } else if (language == 'zh') {
-        locale = localeList[0];
-    }
-    localStorage.setItem('timezone', locale.timezone);
-    localStorage.setItem('language', locale.language);
-    localStorage.setItem('languageText', locale.languageText);
-    window.location.reload();
+	const language = localStorage.getItem('language');
+	let locale = {};
+	if (language == 'en') {
+		locale = localeList[1];
+	} else if (language == 'zh') {
+		locale = localeList[0];
+	}
+	localStorage.setItem('timezone', locale.timezone);
+	localStorage.setItem('language', locale.language);
+	localStorage.setItem('languageText', locale.languageText);
+	window.location.reload();
 };
 
 class Drawers extends React.Component {
@@ -60,9 +69,9 @@ class Drawers extends React.Component {
 	}
 
 	componentDidMount() {
-		console.log(this.props);
+		// console.log(this.props);
 	}
-    // 多语言切换
+	// 多语言切换
 
 	render() {
 		return (
