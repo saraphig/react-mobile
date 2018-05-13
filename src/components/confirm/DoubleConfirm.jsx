bea60026efe045ee88a0c80ff0c5b9fe6e95e528
ShowPenZ <1226703023@qq.com>
2018-05-13 13:39:59 +0800

    modify

--- src/components/confirm/DoubleConfirm.jsx
+++ src/components/confirm/DoubleConfirm.jsx
@@ -12,30 +12,34 @@ import {
 	Validate,
 	TopTabs
 } from '../comComponent/common';
-import { Drawer, List } from 'antd-mobile';
+import { List } from 'antd-mobile';
 
 class ConfirmG2FComp extends React.Component {
 	constructor(props) {
 		super(props);
-		let checked = ''
+		let checked = '';
 		if (!props.info.isGoogle) {
-			checked = 'phone'
+			checked = 'phone';
 		} else {
-			checked = 'google'
+			checked = 'google';
 		}
 		this.state = {
 			validate: '',
 			googleCode: '',
 			checked: checked
-		}
+		};
 		// console.log(props.info)
 	}
 
 	componentDidMount() {}
 
 	// 提交
-	onClick(){
-		this.props.onClick(this.state.googleCode, this.state.validate, this.state.checked)
+	onClick() {
+		this.props.onClick(
+			this.state.googleCode,
+			this.state.validate,
+			this.state.checked
+		);
 	}
 
 	render() {
@@ -43,65 +47,49 @@ class ConfirmG2FComp extends React.Component {
 			intl: { formatMessage }
 		} = this.props;
 
-		// const content = (
-		// 	<div className="doubleConfirm-middleContent">
-		// 		<MidText
-		// 			text={formatMessage({ id: 'doubleConfirm' })}
-		// 			className="midText-doubleConfirm-transfrom"
-		// 		/>
-		// 		<TopTabs
-		// 			placeholder1={formatMessage({ id: 'register.phone' })}
-		// 			placeholder2={formatMessage({
-		// 				id: 'register.phoneValidate'
-		// 			})}
-		// 			placeholder3={formatMessage({
-		// 				id: 'google.code'
-		// 			})}
-		// 		/>
-		// 		<Buttons
-		// 			className="buttons-doubleConfirm-transform"
-		// 			buttonText={formatMessage({
-		// 				id: 'depositWithdraw.submit'
-		// 			})}
-		// 		/>
-		// 	</div>
-		// );
+		const content = (
+			<div className="doubleConfirm-middleContent">
+				<MidText
+					text={formatMessage({ id: 'doubleConfirm' })}
+					className="midText-doubleConfirm-transfrom"
+				/>
+				<TopTabs
+					googleHolder={formatMessage({ id: 'google.code' })}
+					phoneHolder={formatMessage({
+						id: 'register.phone'
+					})}
+					validateHolder={formatMessage({
+						id: 'register.phoneValidate'
+					})}
+					validateChange={val => {
+						this.setState({ validate: val });
+					}}
+					googleChange={val => {
+						this.setState({ googleCode: val });
+					}}
+					checkChange={val => {
+						this.setState({ checked: val });
+					}}
+					info={this.props.info}
+					sendPhoneCode={this.props.sendPhoneCode}
+				/>
+				<Buttons
+					className="buttons-doubleConfirm-transform"
+					buttonText={formatMessage({
+						id: 'depositWithdraw.submit'
+					})}
+					_onClick={() => this.onClick()}
+				/>
+			</div>
+		);
 		return (
 			<div className="DoubleConfirm">
-				<Header _onClick={this.props._onOpenChange}/>
-				<Drawer
-					className="my-drawer"
-					style={{ minHeight: document.documentElement.clientHeight }}
-					enableDragHandle={true}
-					dragToggleDistance={0}
-					position="right"
-					// sidebar={sidebars}
-					open={this.props._open}
-					onOpenChange={this.props._onOpenChange}
-					sidebarStyle={{ background: '#1B1B1B' }}
-				>
-					<div className="doubleConfirm-middleContent">
-						<MidText
-              text={formatMessage({id: 'doubleConfirm'})}
-							className="midText-doubleConfirm-transfrom"
-						/>
-						<TopTabs 
-							googleHolder={formatMessage({id: 'google.code'})}
-							phoneHolder={formatMessage({id: 'register.phone'})}
-							validateHolder={formatMessage({id: 'register.phoneValidate'})}
-							validateChange={(val) => {this.setState({validate: val})}}
-							googleChange={(val) => {this.setState({googleCode: val})}}
-							checkChange={(val) => {this.setState({checked: val})}}
-							info={this.props.info}
-							sendPhoneCode={this.props.sendPhoneCode}
-						/>
-						<Buttons
-							className="buttons-doubleConfirm-transform"
-							buttonText={formatMessage({id: 'depositWithdraw.submit'})}
-							_onClick={() => this.onClick()}
-						/>
-					</div>
-				</Drawer>
+				<Header _onClick={this.props._onOpenChange} />
+				<Drawers
+					childrenNode={content}
+					_onOpenChange={this.props._onOpenChange}
+					_open={this.props._open}
+				/>
 			</div>
 		);
 	}
