aaf3eef91664ff82a8d75a837aedc2bb9fdd8ec8
zds <1030964891@qq.com>
2018-05-16 15:00:04 +0800

    注册页面无感验证在英文显示出中文修改

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -68,7 +68,8 @@ class RegisterComp extends React.Component {
 			{
 				captchaId: dun.captchaId,
 				element: '#dun',
-				lang: 'zh-CN',
+				//lang: 'zh-CN',
+        lang: formatMessage({ id: 'c.dun.lang' }),
 				onReady: instance => {
 					this.captchaIns = instance;
 					// 验证码一切准备就绪，此时可正常使用验证码的相关功能
