545bd0ce82cdc6f940ba8e5168bda7c49362fe73
zds <1030964891@qq.com>
2018-05-16 17:40:38 +0800

    修改中英文包

--- src/locale/zh_CN.js
+++ src/locale/zh_CN.js
@@ -692,6 +692,7 @@ const zh_CN = {
   'serverError': '当前网络异常，请稍后重试',
   'goPC': '请前往pc端绑定谷歌或手机',
   code_117: '验证已超过5分钟时限，请重新登录',
-  'noAgreement': '您并未同意协议'
+  'noAgreement': '您并未同意协议',
+  code_106: '验证码已发送，10分钟内有效，请勿频繁请求'
 };
 export default zh_CN;
