626eb20d8ef9f0bbaf47ad1261988d21982240fa
lhy <lhy@henzfin.com>
2018-06-29 19:30:00 +0800

    增加code_13008

--- src/locale/en_US.js
+++ src/locale/en_US.js
@@ -746,6 +746,7 @@ const enUS = {
 	'candyDetail.nodata': 'No Data',
 	'candyDetail.inviteDetail': 'Inviting details',
 	'candyDetail.specialLink':
-		'\nGood News! TOP.ONE will start Trans-Fee Mining on June.29.\n1. 60% of ONE will be produced by Trans-Fee Mining.\n2. 25% of ONE will be locked for distribution.\n3. The issue price of ONE is 0.03 USDT.\n4. 20% award for early birds.\n5. 8000 Points as award for single\n'
+		'\nGood News! TOP.ONE will start Trans-Fee Mining on June.29.\n1. 60% of ONE will be produced by Trans-Fee Mining.\n2. 25% of ONE will be locked for distribution.\n3. The issue price of ONE is 0.03 USDT.\n4. 20% award for early birds.\n5. 8000 Points as award for single\n',
+	code_13008: 'The phone number has been pre registered, can not repeat invitation twice, please delete the invite code.',
 };
 export default enUS;
